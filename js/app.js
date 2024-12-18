70616ddf5c283200cbe56ca706f5c2dc81ad00d9
Narcolino <marcosanto5550123@outlook.com>
2024-12-18 19:50:24 -0400

    corrigindo logica

--- js/app.js
+++ js/app.js
@@ -1,30 +1,31 @@
 function comprar() {
     let opcoes = document.getElementById('tipo-ingresso').value;
-    let pista = document.getElementById('qtd-pista').innerHTML;
-    let inferior = document.getElementById('qtd-inferior').innerHTML;
-    let superior = document.getElementById('qtd-superior').innerHTML;
-    let quantidade = document.getElementById('qtd').value;
+    let pista = parseInt(document.getElementById('qtd-pista').innerHTML);
+    let inferior = parseInt(document.getElementById('qtd-inferior').innerHTML);
+    let superior = parseInt(document.getElementById('qtd-superior').innerHTML);
+    let quantidade = parseInt(document.getElementById('qtd').value);
 
-    if (opcoes == 'superior'){
-        if(superior == 0){
-            alert('Ingressos esgotados para a categoria cadeira superior! ')
+    if (opcoes == 'superior') {
+        if(superior < quantidade){
+            alert('Ingressos esgotados ou indisponiveis para a categoria cadeira superior! ')
         }else{
             document.getElementById('qtd-superior').innerHTML = superior - quantidade;
+            alert('Compra realizada com sucesso');
         }
-    }else{
-        if (opcoes == 'inferior') {
-            if(inferior == 0){
-                alert('Ingressos esgotados para a categoria cadeira Inferior! ')
-            }else{
-                document.getElementById('qtd-inferior').innerHTML = inferior - quantidade;
-            }
+    } else if (opcoes == 'inferior') {
+        if(inferior < quantidade){
+            alert('Ingressos esgotados  ou indisponiveis para a categoria cadeira Inferior! ')
         }else{
-            if (opcoes == 'pista') {
-                if(pista == 0){
-                    alert('Ingressos esgotados para a categoria Pista! ')
-                }else{
-                 document.getElementById('qtd-pista').innerHTML = pista - quantidade;
-                }
+            document.getElementById('qtd-inferior').innerHTML = inferior - quantidade;
+            alert('Compra realizada com sucesso');
+        }
+    } else {
+        if (opcoes == 'pista') {
+            if(pista < quantidade){
+                alert('Ingressos esgotados  ou indisponiveis para a categoria Pista! ')
+            }else{
+             document.getElementById('qtd-pista').innerHTML = pista - quantidade;
+             alert('Compra realizada com sucesso');
             }
         }
     }
